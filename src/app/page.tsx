cbc62b7780b32e10d15db68c6b8ef6f314ffa7f6
Gerson Lamela <73965255+gersonlamela@users.noreply.github.com>
2023-04-21 15:36:51 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,124 +1,131 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import {AiOutlineCheck, AiOutlineInfoCircle} from 'react-icons/ai';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main
+			className="flex min-h-screen flex-row items-center justify-center 
+      gap-8
+      flex-wrap my-6 xl:mt-0
+">
+			<div className="w-[384px] md:h-[454px] bg-white rounded-[10px]  border border-gray-200 hover:border-purple-400 px-8 pt-12 pb-8 ">
+				<div className="flex flex-col gap-8">
+					<div className="flex flex-col gap-2">
+						<h1 className="uppercase text-purple-600 text-sm font-semibold ">Para você começar</h1>
+						<div className="flex flex-row justify-between items-center">
+							<h2 className="text-gray-800 text-[32px] font-bold ">Essentials</h2>
+							<h3 className="text-gray-800 text-lg font-normal">
+								<span className="text-gray-800 text-[24px] font-bold">
+									19,97 <span className="text-lg"> €/mês</span>
+								</span>
+							</h3>
+						</div>
+					</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+					<button className="w-full text-gray-800 hover:text-white hover:bg-gray-900 border border-gray-800 rounded-[4px] px-6 py-4 flex items-center justify-center">
+						<span className=" font-bold text-base  ">Assinar agora</span>
+					</button>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+					<hr />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+					<div className="flex flex-col gap-4">
+						<div className="flex">
+							<div className="flex items-center  w-full gap-2">
+								<AiOutlineCheck size={20} className="text-green-800" />
+								<span>Até 3 usuários</span>
+							</div>
+							<AiOutlineInfoCircle size={20} className="text-gray-600" />
+						</div>
+						<div className="flex">
+							<div className="flex items-center  w-full gap-2">
+								<AiOutlineCheck size={20} className="text-green-800" />
+								<span>Autoatendimento</span>
+							</div>
+							<AiOutlineInfoCircle size={20} className="text-gray-600" />
+						</div>
+					</div>
+				</div>
+			</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+			<div className="relative w-[384px] md:h-[454px] bg-gray-900 rounded-[10px]  border border-gray-200 hover:border-purple-400 px-8 pt-12 pb-8  before:content-['Mais_Vantajoso'] before:font-bold before:text-[12px] before:inline-block before:bg-orange-400 before:rounded-[6px] before:absolute before:left-1/2 before:-top-[17px] before:uppercase before:py-2 before:px-4 before:-translate-x-1/2">
+				<div className="flex flex-col gap-8">
+					<div className="flex flex-col gap-2 ">
+						<h1 className="uppercase text-purple-600 text-sm font-semibold ">Para você decolar</h1>
+						<div className="flex flex-row justify-between items-center">
+							<h2 className="text-white text-[32px] font-bold ">Ultimate </h2>
+							<h3 className="text-white text-lg font-normal">
+								<span className="text-white text-[24px] font-bold">
+									29,97<span className="text-lg"> €/mês</span>
+								</span>
+							</h3>
+						</div>
+					</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+					<button className="w-full text-white bg-purple-400  hover:bg-purple-500 border border-gray-800 rounded-[4px] px-6 py-4 flex items-center justify-center">
+						<span className=" font-bold text-base "> Assinar agora</span>
+					</button>
+
+					<hr className="bg-gray-700 text-gray-700" />
+
+					<div className="flex flex-col gap-4 text-gray-300">
+						<div className="flex">
+							<div className="flex items-center  w-full gap-2">
+								<AiOutlineCheck size={20} className="text-green-800" />
+								<span>Usuários ilimitados</span>
+							</div>
+							<AiOutlineInfoCircle size={20} className="text-gray-600" />
+						</div>
+						<div className="flex">
+							<div className="flex items-center  w-full gap-2">
+								<AiOutlineCheck size={20} className="text-green-800" />
+								<span>Suporte 24/7</span>
+							</div>
+							<AiOutlineInfoCircle size={20} className="text-gray-600" />
+						</div>
+
+						<div className="flex">
+							<div className="flex items-center  w-full gap-2">
+								<AiOutlineCheck size={20} className="text-green-800" />
+								<span>CSM Dedicado</span>
+							</div>
+							<AiOutlineInfoCircle size={20} className="text-gray-600" />
+						</div>
+
+						<div className="flex">
+							<div className="flex items-center  w-full gap-2">
+								<AiOutlineCheck size={20} className="text-green-800" />
+								<span>Treinamentos</span>
+							</div>
+							<AiOutlineInfoCircle size={20} className="text-gray-600" />
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="w-[384px] md:h-[454px] bg-white rounded-[10px]  border border-gray-200 hover:border-purple-400 px-8 pt-12 pb-8 ">
+				<div className="flex flex-col gap-8">
+					<div className="flex flex-col gap-2">
+						<h1 className="uppercase text-purple-600 text-sm font-semibold ">Para sua empresa</h1>
+						<div className="flex flex-row justify-between items-center">
+							<h2 className="text-gray-800 text-[32px] font-bold ">Enterprise</h2>
+						</div>
+					</div>
+
+					<button className="w-full text-gray-800 hover:text-white hover:bg-gray-900 border border-gray-800 rounded-[4px] px-6 py-4 flex items-center justify-center">
+						<span className=" font-bold text-base  "> Fale com a gente</span>
+					</button>
+
+					<hr />
+
+					<div className="flex items-center flex-row gap-2">
+						<div>
+							<AiOutlineCheck className="text-green-800 w-5 h-5" />
+						</div>
+						<div>
+							<span className="text-base">Plano customizado especialmente para a necessidade de seu negocio</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
